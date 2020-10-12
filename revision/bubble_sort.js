b3b9a44e1786876aa7dcfcc2d2d3fd84199b16a5
= <saleh.majeed8@gmail.com>
2020-10-12 17:56:07 +0530

    bubble sort using recursion

---
+++ revision/bubble_sort.js
@@ -0,0 +1,20 @@
+function sorts(numbers) {
+    let i = 0;
+    function wrapper() {
+        if (numbers[i] > numbers[i + 1]) {
+            ([numbers[i], numbers[i + 1]] = numbers[i + 1]), numbers[i];
+        }
+
+        i++;
+
+        if (i == numbers.length - 1) i = 0;
+
+        if (String(numbers) != String(numbers.sort((a, b) => a - b))) wrapper();
+
+        return numbers;
+    }
+    return wrapper();
+}
+
+// console.log(sorts([1, 24, 6, 2, 3])); // [1, 2, 3, 6, 24]
+console.log(sorts([2, 8, 1, 3, 6, 7, 5, 4])); // [1, 2, 3, 4, 5, 6, 7, 8]
